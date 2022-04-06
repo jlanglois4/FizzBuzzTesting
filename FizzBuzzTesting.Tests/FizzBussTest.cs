f4a6b0ee6cc4ef6bb32071438b45fa8b88cd8bc1
jlanglois4 <jlanglois4@my.wctc.edu>
2022-04-05 19:45:08 -0500

    Did tests 1-5, 15, 30.

--- FizzBuzzTesting.Tests/FizzBussTest.cs
+++ FizzBuzzTesting.Tests/FizzBussTest.cs
@@ -1,14 +1,52 @@
 using FizzBuzzTesting;
 using Xunit;
-
-namespace FizzBuzzTesting.Tests;
+using Assert = Xunit.Assert;
 
 public class FizzBuzzTest
 {
+    FizzBuzz fb = new FizzBuzz();
+    
     [Fact]
-    public void canMakeClass()
+    public void One()
     {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
+        Assert.Equal("1",fb.DoFizzBuzz(1));
     }
+
+    [Fact]
+    public void Two()
+    {
+        Assert.Equal("2", fb.DoFizzBuzz(2));
+    }
+
+    [Fact]
+    public void Three()
+    {
+        Assert.Equal("Fizz", fb.DoFizzBuzz(3));
+    }
+    
+    [Fact]
+    public void Four()
+    {
+        Assert.Equal("4", fb.DoFizzBuzz(4));
+    }
+    
+    [Fact]
+    public void Five()
+    {
+        Assert.Equal("Buzz", fb.DoFizzBuzz(5));
+    }
+
+    [Fact]
+    public void Fifteen()
+    {
+        Assert.Equal("FizzBuzz", fb.DoFizzBuzz(15));
+    }
+    
+    [Fact]
+    public void Thirty()
+    {
+        Assert.Equal("FizzBuzz", fb.DoFizzBuzz(30));
+    }
+
+
 }
--- FizzBuzzTesting/FizzBuzz.cs
+++ FizzBuzzTesting/FizzBuzz.cs
@@ -2,6 +2,24 @@
 {
     public class FizzBuzz
     {
-        
+        public string DoFizzBuzz(int n)
+        {
+            if (n % 3 == 0 && n % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            
+            if (n % 3 == 0)
+            {
+                return "Fizz";
+            }
+
+            if (n % 5 == 0)
+            {
+                return "Buzz";
+            }
+
+            return n.ToString();
+        }
     }
 }
